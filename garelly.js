7e55aeeaaff91d3a985b5badf1668a7770c5abfc
K S Keerthana <ksk66117@gmail.com>
2026-03-08 21:28:45 +0530

    Add files via upload
    Developed a responsive image gallery using HTML, CSS, and JavaScript

---
+++ garelly.js
@@ -0,0 +1,29 @@
+let images = document.querySelectorAll(".gallery img");
+let lightbox = document.getElementById("lightbox");
+let lightboxImg = document.getElementById("lightbox-img");
+
+let currentIndex = 0;
+
+function openLightbox(index){
+    currentIndex = index;
+    lightbox.style.display = "flex";
+    lightboxImg.src = images[index].src;
+}
+
+function closeLightbox(){
+    lightbox.style.display = "none";
+}
+
+function changeImage(step){
+    currentIndex += step;
+
+    if(currentIndex < 0){
+        currentIndex = images.length - 1;
+    }
+
+    if(currentIndex >= images.length){
+        currentIndex = 0;
+    }
+
+    lightboxImg.src = images[currentIndex].src;
+}
